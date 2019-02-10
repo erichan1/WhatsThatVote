d03fa9a7d4ac6cd1795e748b79d19733ee94af75
Eric Han <eshan@caltech.edu>
2019-02-09 18:31:36 -0800

    tuned NN to 77.8% test acc and mostly wrote func to train and test data on entire dataset

--- neural_network_script.py
+++ neural_network_script.py
@@ -15,6 +15,7 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, BatchNormalization
 from keras import regularizers
 from sklearn.model_selection import KFold
 from keras.utils import to_categorical
+from sklearn.metrics import roc_auc_score
 
 #Define functions
 def load_data(filename, skiprows = 1):
@@ -76,51 +77,94 @@ def cross_val_NN(model, x_data, y_data):
         model.set_weights(weights)
         x_train_fold, x_test_fold = x_data[train_index], x_data[test_index]
         y_train_fold, y_test_fold = y_data[train_index], y_data[test_index]
-        training_model = model.fit(x_train_fold, y_train_fold, epochs=10, batch_size=1000)
+        training_model = model.fit(x_train_fold, y_train_fold, epochs=30, batch_size=1000)
         training_accuracy.append(model.evaluate(x=x_train_fold, y=y_train_fold)[1])
         test_accuracy.append(model.evaluate(x=x_test_fold,y=y_test_fold)[1])
+        
+
     training_accuracy = np.array(training_accuracy)
     test_accuracy = np.array(test_accuracy)
 
     return (training_accuracy, test_accuracy)
 
-train_data = load_data('train_2008.csv')
-test_data = load_data('test_2008.csv')
-y_train = train_data[:,382]
-x_train = train_data[:,3:382] #Here I remove the first 3 columns representing ID, month, and year
-x_test = test_data[:,3:] #Here I remove the first 3 columns representing ID, month, and year
+def create_NN(input_size):
+    # encapsulate the model I've built so far
+    # if you want to play w model parameters do it here
+    # may add parameters to this function so I can do sensitivity training
 
-x_train_reduced = data_reduction(x_train, 0.98)
-print(x_train.shape)
-print(x_train_reduced.shape)
+    model = Sequential()
+    model.add(Dense(input_size, input_shape=(input_size,)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu')) 
+    model.add(Dropout(0.4))
+    
+    model.add(Dense(200))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+    
+    model.add(Dense(2))
+    model.add(Activation('softmax'))
+    
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
-print(x_train_reduced)
-x_train_reduced = normalize_data(x_train_reduced)
+    return model
 
-input_size = x_train_reduced.shape[1]
-y_train_binary = to_categorical(y_train)
+def cross_val_test():
+    # this isn't really a function. 
+    # This encapsulates the code to get cross val in its own block
+    # Call this in the main function. 
 
-# create model
-model = Sequential()
-model.add(Dense(input_size, input_shape=(input_size,)))
-model.add(BatchNormalization())
-model.add(Activation('relu')) 
-model.add(Dropout(0.3))
+    train_data = load_data('train_2008.csv')
+    test_data = load_data('test_2008.csv')
+    y_train = train_data[:,382]
+    x_train = train_data[:,3:382] #Here I remove the first 3 columns representing ID, month, and year
+    
+    x_train_reduced = data_reduction(x_train, 0.98)
+    print(x_train.shape)
+    print(x_train_reduced.shape)
+    
+    x_train_reduced = normalize_data(x_train_reduced)
+    
+    input_size = x_train_reduced.shape[1]
+    y_train_binary = to_categorical(y_train)
+    model = create_NN(input_size)
+    
+    # Cross val to get an idea of accuracies.
+    train_acc, test_acc = cross_val_NN(model, x_train_reduced, y_train_binary)
+    
+    print('training_accuracy {}'.format(np.mean(train_acc)))
+    print('test_accuracy {}'.format(np.mean(test_acc)))
 
-model.add(Dense(200))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dropout(0.3))
+def final_test():
+    # also not really a function
+    # encapsulates the process of training the final model and using it on test data
 
-model.add(Dense(2))
-model.add(Activation('softmax'))
+    train_data = load_data('train_2008.csv')
+    test_data = load_data('test_2008.csv')
+    y_train = train_data[:,382]
+    x_train = train_data[:,3:382] #Here I remove the first 3 columns representing ID, month, and year
+    x_test = test_data[:,3:382] #Here I remove the first 3 columns representing ID, month, and year
+    
+    x_train_reduced = data_reduction(x_train, 0.98)
+    print(x_train.shape)
+    print(x_train_reduced.shape)
+    
+    x_train_reduced = normalize_data(x_train_reduced)
+    
+    input_size = x_train_reduced.shape[1]
+    y_train_binary = to_categorical(y_train)
+    model = create_NN()
+    
+    trained_model = model.fit(x_train, y_train_binary, epochs=30, batch_size=1000)
 
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    Y_pred = trained_model.predict(x_test)
 
-# Cross val to get an idea of accuracies.
-train_acc, test_acc = cross_val_NN(model, x_train_reduced, y_train_binary)
+    # write predictions
 
-print('training_accuracy {}'.format(np.mean(train_acc)))
-print('test_accuracy {}'.format(np.mean(test_acc)))
+if __name__ == '__main__':
+    # cross_val_test() 
+    final_test()
+    
